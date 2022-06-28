933b8b98e20a9b7d0d6891d746da9f93c450a66b
aktgram <86195125+aktgram@users.noreply.github.com>
2022-06-29 01:19:51 +0530

    Add files via upload

---
+++ code/4.a/server.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<ctype.h>
+#include<time.h>
+
+void main() 
+{
+  char *ip = "127.0.0.1";
+  int port = 5543;
+
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[1024];
+  socklen_t addr_size;
+  int n;
+
+  sockfd = socket(AF_INET,SOCK_DGRAM,0);
+  if(sockfd<0) {
+    perror("[-]Socket error");
+    exit(1);
+  }
+
+  memset(&server_addr,'\0',sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  n = bind(sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+  if(n<0){
+    perror("[+]Bind error");
+    exit(1);
+  }
+
+  bzero(buffer,1024);
+  addr_size = sizeof(client_addr);
+  while(1)
+  {
+    recvfrom(sockfd,buffer,1024,0,(struct sockaddr*)&client_addr,&addr_size);
+    printf("Client connected.\n");
+    printf("[+]Data from client: %s\n",buffer);
+
+    time_t t;
+    time(&t);
+    strcpy(buffer,ctime(&t));
+
+    sendto(sockfd,buffer,1024,0,(struct sockaddr*)&client_addr,sizeof(client_addr));
+  }
+}
+
